7728cef51a7cb93ed82f706f68176fb52d3b83aa
Forrest Collman <forrest.collman@gmail.com>
2018-07-13 15:22:07 -0700

    finished bulk insert

--- annotationengine/annotation.py
+++ annotationengine/annotation.py
@@ -6,6 +6,7 @@ from emannotationschemas.errors import UnknownAnnotationTypeException
 import numpy as np
 import json
 from functools import partial
+import pandas as pd
 
 bp = Blueprint("annotation", __name__, url_prefix="/annotation")
 
@@ -37,12 +38,12 @@ def bsp_import_fn(cv, item):
     item['supervoxel_id'] = svid
 
 
-def get_schema_with_context(annotation_type, dataset):
+def get_schema_with_context(annotation_type, dataset, flatten=False):
     dataset_db = get_dataset_db()
     cv = dataset_db.get_cloudvolume(dataset)
     myp = partial(bsp_import_fn, cv)
 
-    context = {'bsp_fn': myp}
+    context = {'bsp_fn': myp, 'flatten': flatten}
     Schema = get_schema(annotation_type)
     schema = Schema(context=context)
     return schema
@@ -53,36 +54,70 @@ def get_annotation_types(dataset):
     return get_schemas()
 
 
+def nest_dictionary(d, key_path=None, sep="."):
+    if key_path is None:
+        key_path = []
+    d_new = {}
+    for k, v in d.items():
+        sub_keys = k.split(sep)
+        ds = d_new
+        for sub_key in sub_keys[:-1]:
+            if sub_key not in ds.keys():
+                ds[sub_key] = {}
+            ds = ds[sub_key]
+        ds[sub_keys[-1]] = v
+    return d_new
+
+
+def import_dataframe(db, dataset, annotation_type, schema, df):
+    annotations = []
+    for k, row in df.iterrows():
+        d = nest_dictionary(dict(row))
+        d['type'] = annotation_type
+        result = schema.load(d)
+        if len(result.errors) > 0:
+            abort(422, result.errors)
+        ann = result.data
+        supervoxels = collect_supervoxels(ann)
+        blob = json.dumps(schema.dump(ann).data)
+        annotations.append((supervoxels, blob))
+
+    return annotations
+
+
 @bp.route("/dataset/<dataset>/<annotation_type>", methods=["POST"])
 def import_annotations(dataset, annotation_type):
     db = get_db()
+    is_bulk = request.args.get('bulk', 'false') == 'true'
 
     if request.method == "POST":
+        user_id = jsonify(origin=request.headers.get('X-Forwarded-For',
+                                                     request.remote_addr))
         # iterate through annotations in json posted
         try:
-            schema = get_schema_with_context(annotation_type, dataset)
+            schema = get_schema_with_context(annotation_type,
+                                             dataset)
         except UnknownAnnotationTypeException as m:
             abort(404, str(m))
         d = request.json
-        result = schema.load(d, many=True)
-        if len(result.errors) > 0:
-            abort(422, result.errors)
+        if is_bulk:
+            df = pd.read_json(d)
+            annotations = import_dataframe(db, dataset,
+                                           annotation_type, schema, df)
+        else:
+            result = schema.load(d, many=True)
+            if len(result.errors) > 0:
+                abort(422, result.errors)
 
-        user_id = jsonify(origin=request.headers.get('X-Forwarded-For',
-                                                     request.remote_addr))
-
-        annotations = []
-        for ann in result.data:
-            supervoxels = collect_supervoxels(ann)
-            blob = json.dumps(schema.dump(ann).data)
-            annotations.append((supervoxels, blob))
-        print("dataset", dataset, "annotation_type", annotation_type)
-        print("inserting", blob, 'sv_ids', supervoxels)
+            annotations = []
+            for ann in result.data:
+                supervoxels = collect_supervoxels(ann)
+                blob = json.dumps(schema.dump(ann).data)
+                annotations.append((supervoxels, blob))
         uids = db.insert_annotations(dataset,
                                      annotation_type,
                                      annotations,
                                      user_id)
-        print("uids", uids)
         return jsonify(np.uint64(uids).tolist())
 
 
