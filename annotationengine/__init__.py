c470e4c901243ff0688cae708a4a53073f77db3c
sdorkenw <sven.dorkenwald@googlemail.com>
2018-07-31 13:19:45 -0700

    added pychunkedgraph blueprint to the server init

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -5,6 +5,13 @@ from annotationengine import annotation
 from annotationengine import schemas
 from annotationengine import voxel
 from annotationengine import dataset as dataset_mod
+
+try:
+    from pychunkedgraph.app import app_blueprint as pychg_app_blueprint
+    pychg_avail = True
+except:
+    pychg_avail = False
+
 __version__ = "0.0.1"
 
 
@@ -24,6 +31,37 @@ def create_app(test_config=None):
     app.register_blueprint(voxel.bp)
     app.register_blueprint(schemas.bp)
     app.register_blueprint(dataset_mod.bp)
+    with app.app_context():
+        db = annotation.get_db()
+        types = schemas.get_types()
+        for dataset in app.config['DATASETS']:
+            for type_ in types:
+                if not db.has_table(dataset['name'], type_):
+                    db.create_table(dataset['name'], type_)
+                    print('creating table {}:{}'.format(dataset['name'],
+                                                        type_))
+                else:
+                    print('table exists {} {}'.format(dataset['name'], type_))
+    return app
+
+
+def create_app_seg(test_config=None):
+
+    # Define the Flask Object
+    app = Flask(__name__,
+                instance_path=get_instance_folder_path(),
+                instance_relative_config=True)
+    # load configuration (from test_config if passed)
+    if test_config is None:
+        app = configure_app(app)
+    else:
+        app.config.update(test_config)
+    # register blueprints
+    app.register_blueprint(annotation.bp)
+    app.register_blueprint(voxel.bp)
+    app.register_blueprint(schemas.bp)
+    app.register_blueprint(dataset_mod.bp)
+    app.register_blueprint(pychg_app_blueprint.bp)
     with app.app_context():
         db = annotation.get_db()
         types = schemas.get_types()
--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 # Run a test server.
-from annotationengine import create_app
-app = create_app()
+import sys
+from annotationengine import create_app, create_app_seg
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000, debug=True)
+
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "--seg":
+            app = create_app_seg()
+        else:
+            raise Exception("Unknow parameter %s" % sys.argv[2])
+    elif len(sys.argv) == 2:
+        app = create_app()
+    else:
+        raise Exception("Wrong number of parameters")
+
+    app.run(host='0.0.0.0', port=4001, debug=True)
