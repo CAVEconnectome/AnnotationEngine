52959915b972de3b71098a8474720d22a2440e12
sdorkenw <sven.dorkenwald@googlemail.com>
2018-08-05 23:44:55 +0200

    extended the annotationclient

--- annotationengine/annotationclient.py
+++ annotationengine/annotationclient.py
@@ -1,5 +1,12 @@
 import requests
 import os
+import numpy as np
+import cloudvolume
+import json
+import time
+
+from emannotationschemas.utils import get_flattened_bsp_keys_from_schema
+from emannotationschemas import get_schema
 
 
 class AnnotationClient(object):
@@ -26,7 +33,18 @@ class AnnotationClient(object):
         assert(response.status_code == 200)
         return response
 
-    def bulk_import_df(self, dataset_name, annotation_type, data_df):
+    def get_dataset(self, dataset_name):
+        """ Returns information about the dataset
+
+        :return: dict
+        """
+        url = "{}/dataset/{}".format(self.endpoint, dataset_name)
+        response = self.session.get(url, verify=False)
+        assert(response.status_code == 200)
+        return response
+
+    def bulk_import_df(self, dataset_name, annotation_type, data_df,
+                       block_size=10000):
         """ Imports all annotations from a single dataframe in one go
 
         :param dataset_name: str
@@ -34,9 +52,52 @@ class AnnotationClient(object):
         :param data_df: pandas DataFrame
         :return:
         """
+        dataset_info = json.loads(self.get_dataset(dataset_name).content)
+        cv = cloudvolume.CloudVolume(dataset_info["CV_SEGMENTATION_PATH"])
+        chunk_size = np.array(cv.info["scales"][0]["chunk_sizes"][0]) * 8
+        bounds = np.array(cv.bounds.to_list()).reshape(2, 3)
+
+        Schema = get_schema(annotation_type)
+        schema = Schema()
+
+        rel_column_keys = get_flattened_bsp_keys_from_schema(schema)
+
+        data_df = data_df.reset_index(drop=True)
+
+        bspf_coords = []
+        for rel_column_key in rel_column_keys:
+            bspf_coords.append(np.array(data_df[rel_column_key].values.tolist())[:, None, :])
+
+        bspf_coords = np.concatenate(bspf_coords, axis=1)
+        bspf_coords -= bounds[0]
+        bspf_coords = (bspf_coords / chunk_size).astype(np.int)
+
+        bspf_coords = bspf_coords[:, 0]
+        ind = np.lexsort((bspf_coords[:, 0], bspf_coords[:, 1], bspf_coords[:, 2]))
+
+        data_df = data_df.reindex(ind)
+
         url = "{}/annotation/dataset/{}/{}?bulk=true".format(self.endpoint,
                                                              dataset_name,
                                                              annotation_type)
-        response = self.session.post(url, json=data_df.to_json())
-        assert(response.status_code == 200)
-        return response.json
+        n_blocks = int(np.ceil(len(data_df) / block_size))
+
+        print("Number of blocks: %d" % n_blocks)
+        time_start = time.time()
+
+        responses = []
+        for i_block in range(0, len(data_df), block_size):
+            if i_block > 0:
+                dt = time.time() - time_start
+                eta = dt / i_block * len(data_df) - dt
+                print("%d / %d - dt = %.2fs - eta = %.2fs" %
+                      (i_block, len(data_df), dt, eta))
+
+            data_block = data_df[i_block: i_block + block_size].to_json()
+            response = self.session.post(url, json=data_block, verify=False)
+            assert(response.status_code == 200)
+            responses.append(response.json)
+
+
+
+        return responses
