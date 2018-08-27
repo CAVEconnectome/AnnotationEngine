e31a6fcdcf6c6f40b44a453849d12845b22bb0c0
sdorkenw <sven.dorkenwald@googlemail.com>
2018-08-27 14:20:06 -0700

    deployment to google cloud app engine

--- run.py
+++ run.py
@@ -5,11 +5,12 @@ from annotationengine import create_app
 import os
 
 HOME = os.path.expanduser("~")
-app = create_app()
 
 if __name__ == "__main__":
 
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
+
+    app = create_app()
 
     app.run(host='0.0.0.0',
             port=4001,
