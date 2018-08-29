7d031d6a68a75739310650afffe6bb7ef723f6d6
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-08-29 11:56:00 -0700

    moved app init outside of __main__ part

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ import os
 
 HOME = os.path.expanduser("~")
 
+app = create_app()
+
 if __name__ == "__main__":
 
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
-
-    app = create_app()
 
     app.run(host='0.0.0.0',
             port=4001,
