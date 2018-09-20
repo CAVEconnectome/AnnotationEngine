4370354e0b50b87fc457a7f9dad16ab33efa276d
Forrest Collman <forrest.collman@gmail.com>
2018-09-20 14:34:48 -0700

    remove pychunkgraph

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -12,14 +12,6 @@ def create_app(test_config=None):
     from annotationengine import dataset as dataset_mod
     from annotationengine import chunked_annotation
 
-    try:
-        from pychunkedgraph.app.app_blueprint import bp as cg_bp
-        cg_avail = True
-    except ImportError:
-        cg_avail = False
-
-
-    print('instance folder', get_instance_folder_path())
     # Define the Flask Object
     app = Flask(__name__,
                 instance_path=get_instance_folder_path(),
@@ -34,10 +26,6 @@ def create_app(test_config=None):
     app.register_blueprint(voxel.bp)
     app.register_blueprint(schemas.bp)
     app.register_blueprint(dataset_mod.bp)
-
-    if cg_avail:
-        app.register_blueprint(cg_bp)
-        app.register_blueprint(chunked_annotation.bp)
 
     with app.app_context():
         db = annotation.get_db()
