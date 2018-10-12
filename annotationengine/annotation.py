6035afb92b9c31d9d0f1f5e2f22d0b73334dc5d8
Forrest Collman <forrest.collman@gmail.com>
2018-10-12 13:56:25 -0700

    syncing annotation

--- annotationengine/annotation.py
+++ annotationengine/annotation.py
@@ -11,7 +11,7 @@ from multiwrapper import multiprocessing_utils as mu
 
 bp = Blueprint("annotation", __name__, url_prefix="/annotation")
 
-__version__ = "0.0.8"
+__version__ = "0.0.20"
 
 def collect_supervoxels(d):
     svid_set = collect_supervoxels_recursive(d)
