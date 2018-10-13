f4aaa9b84fbc00a681702907b7d8ca00b96c2b5d
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-10-13 12:13:00 -0700

    Bump version: 0.0.28 → 0.0.29

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.0.28"
+__version__ = "0.0.29"
 
 
 def create_app(test_config=None):
--- annotationengine/annotation.py
+++ annotationengine/annotation.py
@@ -13,7 +13,7 @@ import collections
 
 bp = Blueprint("annotation", __name__, url_prefix="/annotation")
 
-__version__ = "0.0.28"
+__version__ = "0.0.29"
 
 def collect_supervoxels(d):
     svid_set = collect_supervoxels_recursive(d)
