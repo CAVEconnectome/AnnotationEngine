c018238c5941b3a0a0feec87efd8bedd98cf17b6
Travis CI User <travis@example.org>
2018-10-21 16:56:34 +0000

    [skip travis] Bump version: 0.0.43 → 0.0.44

--- annotationengine/annotation.py
+++ annotationengine/annotation.py
@@ -13,7 +13,7 @@ import collections
 
 bp = Blueprint("annotation", __name__, url_prefix="/annotation")
 
-__version__ = "0.0.43"
+__version__ = "0.0.44"
 
 def collect_supervoxels(d):
     svid_set = collect_supervoxels_recursive(d)
