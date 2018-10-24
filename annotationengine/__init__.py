d41f9a0547d74ea4d41386e4db3b713d13f6067e
Travis CI User <travis@example.org>
2018-10-24 16:56:33 +0000

    [travisci skip] Bump version: 0.0.47 → 0.0.48

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.0.47"
+__version__ = "0.0.48"
 
 
 def create_app(test_config=None):
--- annotationengine/annotation.py
+++ annotationengine/annotation.py
@@ -13,7 +13,7 @@ import collections
 
 bp = Blueprint("annotation", __name__, url_prefix="/annotation")
 
-__version__ = "0.0.47"
+__version__ = "0.0.48"
 
 def collect_supervoxels(d):
     svid_set = collect_supervoxels_recursive(d)
