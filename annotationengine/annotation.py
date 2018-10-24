43aa609aab5054d23f7ca042df91371fe6286746
Forrest Collman <forrest.collman@gmail.com>
2018-10-24 10:15:54 -0700

    Bump version: 1.0.0 → 1.0.1

--- annotationengine/annotation.py
+++ annotationengine/annotation.py
@@ -14,7 +14,7 @@ import os
 import requests
 bp = Blueprint("annotation", __name__, url_prefix="/annotation")
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 def collect_bound_spatial_points(ann: dict, schema: dict):
