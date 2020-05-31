aa249da72daf2bbad698dc0094c2c77eb73d8c53
dlbrittain <dlbrittain@gmail.com>
2020-05-31 16:07:39 -0700

    Remove google bigtable client references

--- annotationengine/anno_database.py
+++ annotationengine/anno_database.py
@@ -1,34 +1,10 @@
 from flask import current_app
-from dynamicannotationdb.annodb_meta import AnnotationMetaDB
-from google.auth import credentials, default as default_creds
-from google.cloud import bigtable
-
+from dynamicannotationdb.client import AnnotationDBMeta
 cache = {}
-
-
-class DoNothingCreds(credentials.Credentials):
-    def refresh(self, request):
-        pass
-
-
-def get_client(config):
-    project_id = config.get('project_id', 'pychunkedgraph')
-
-    if config.get('emulate', False):
-        credentials = DoNothingCreds()
-    else:
-        credentials, project_id = default_creds()
-
-    client = bigtable.Client(admin=True,
-                             project=project_id,
-                             credentials=credentials)
-    return client
-
 
 def get_db():
     if 'db' not in cache:
-        cred_config = current_app.config['BIGTABLE_CONFIG']
-        client = get_client(cred_config)
-        cache["db"] = AnnotationMetaDB(client=client)
+        sql_uri_config = current_app.config['SQLALCHEMY_DATABASE_URI']
+        cache["db"] = AnnotationDBMeta(sql_uri=sql_uri_config)
 
     return cache["db"]
