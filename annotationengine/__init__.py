b4861130041f93089d12dd888265a74245dddb88
dlbrittain <dlbrittain@gmail.com>
2020-05-31 16:08:36 -0700

    Import basic flask_restx blueprint with swagger

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, url_for, redirect, Blueprint
 import json
 import numpy as np
 from annotationengine.config import configure_app
 from annotationengine.utils import get_instance_folder_path
-from annotationengine import annotation
+from annotationengine.api import api_bp
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
+import logging
 
 __version__ = "1.0.6"
 
@@ -23,25 +26,22 @@ def create_app(test_config=None):
                 instance_path=get_instance_folder_path(),
                 instance_relative_config=True)
     app.json_encoder = AEEncoder
-    # load configuration (from test_config if passed)
+    
+    logging.basicConfig(level=logging.DEBUG)
+
     if test_config is None:
         app = configure_app(app)
     else:
         app.config.update(test_config)
-    # register blueprints
-    app.register_blueprint(annotation.bp)
 
-    # with app.app_context():
-    #     db = annotation.get_db()
-    #     types = schemas.get_types()
-    #     dataset_db = get_dataset_db()
+    apibp = Blueprint('api', __name__, url_prefix='/annotation/api')
 
-    #     # for dataset_name in dataset_db.get_dataset_names():
-    #     #     for type_ in types:
-    #     #         if not db.has_table(dataset_name, type_):
-    #     #             db.create_table(dataset_name, type_)
-    #     #             print('creating table {}:{}'.format(dataset_name,
-    #     #                                                 type_))
-    #     #         else:
-    #     #             print('table exists {} {}'.format(dataset_name, type_))
+    with app.app_context():
+        api = Api(app, title="Annotation Engine API", version=__version__, doc="/doc")
+        api.add_namespace(api_bp, path='/v2')
+
+    @app.route("/info/health")
+    def health():
+        return jsonify("healthy"), 200
+   
     return app
