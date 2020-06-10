bfdd3798e1206e383a9b67af24ac0f829d11496a
dlbrittain <dlbrittain@gmail.com>
2020-06-10 16:13:08 -0700

    CRUD schemas

--- annotationengine/schemas.py
+++ annotationengine/schemas.py
@@ -8,7 +8,17 @@ class Metadata(Schema):
 
 
 class TableSchema(Schema):
-    em_dataset_name = fields.Str(order=0, required=True)
+    em_dataset = fields.Str(order=0, required=True)
     table_name = fields.Str(order=1, required=True)   
     schema_type = fields.Str(order=2, required=True)
+
+class CreateTableSchema(TableSchema):
     metadata = fields.Nested(Metadata, order=3, required=True)
+
+
+class DeleteAnnotationSchema(TableSchema):
+    annotation_ids = fields.List(fields.Int, required=True)
+    
+
+class PutAnnotationSchema(TableSchema):
+    annotations = fields.List(fields.Dict, required=True)
