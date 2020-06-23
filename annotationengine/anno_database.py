3d951843a25ddf060efd510fc44f626effbe894f
Forrest Collman <forrest.collman@gmail.com>
2020-06-23 07:21:33 -0700

    switch to aligned_volume specific dadb interface

--- annotationengine/anno_database.py
+++ annotationengine/anno_database.py
@@ -2,9 +2,9 @@ from flask import current_app
 from dynamicannotationdb.client import AnnotationDBMeta
 cache = {}
 
-def get_db():
-    if 'db' not in cache:
+def get_db(aligned_volume):
+    if aligned_volume not in cache:
         sql_uri_config = current_app.config['SQLALCHEMY_DATABASE_URI']
-        cache["db"] = AnnotationDBMeta(sql_uri=sql_uri_config)
+        cache[aligned_volume] = AnnotationDBMeta(aligned_volume, sql_uri=sql_uri_config)
 
-    return cache["db"]
+    return cache[aligned_volume]
