2d6d8990ac7520ac844af818f9b260bf8bcb143b
dlbrittain <dlbrittain@gmail.com>
2020-06-25 09:27:37 -0700

    Update Annotation client import

--- annotationengine/anno_database.py
+++ annotationengine/anno_database.py
@@ -1,11 +1,11 @@
 from flask import current_app
-from dynamicannotationdb.client import AnnotationDBMeta
+from dynamicannotationdb.annotation_client import DynamicAnnotationClient
 cache = {}
 
-def get_db(aligned_volume)->AnnotationDBMeta:
+def get_db(aligned_volume)->DynamicAnnotationClient:
     if aligned_volume not in cache:
         sql_uri_config = current_app.config['SQLALCHEMY_DATABASE_URI']
-        cache[aligned_volume] = AnnotationDBMeta(aligned_volume=aligned_volume,
+        cache[aligned_volume] = DynamicAnnotationClient(aligned_volume=aligned_volume,
                                                  sql_base_uri=sql_uri_config)
 
     return cache[aligned_volume]
