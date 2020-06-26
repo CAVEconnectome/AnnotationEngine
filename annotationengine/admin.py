aed8626d6577bacfa29da2d5b0227e009240071d
Forrest Collman <forrest.collman@gmail.com>
2020-06-25 20:44:32 -0700

    wip on admin interface

--- annotationengine/admin.py
+++ annotationengine/admin.py
@@ -1,8 +1,10 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from dynamicannotationdb.models import Metadata
 from middle_auth_client import auth_required
 from flask import g, redirect, url_for
+from middle_auth_client import auth_requires_admin, auth_required
+
 
 class SuperAdminView(ModelView):
      @auth_required
@@ -13,8 +15,24 @@ class SuperAdminView(ModelView):
         # redirect to login page if user doesn't have access
         return redirect(url_for('admin.index'))
 
+# Create customized index view class that handles login & registration
+class MyAdminIndexView(AdminIndexView):
 
-def setup_admin(app, db):
-    admin = Admin(app, name="annotationengine")
-    admin.add_view(ModelView(Metadata, db.session))
+     
+     @expose('/', methods=["GET"])
+     @auth_required
+     def index(self):
+          return super(MyAdminIndexView, self).index()
+
+     @auth_required
+     def is_accessible(self):
+        return True
+
+def setup_admin(app, db, aligned_volume):
+    index_view = MyAdminIndexView(name=f"{aligned_volume}_admin",
+                                  endpoint=f'{aligned_volume}',
+                                  url=f'/info/admin/{aligned_volume}')
+    admin = Admin(name = f'{aligned_volume}_admin2',endpoint=f'{aligned_volume}',index_view=index_view)
+    admin.add_view(SuperAdminView(Metadata, db.session, name=f'{aligned_volume}_metadata'))
+    admin.init_app(app)
     return admin
