2022feb3a853b04528b2b2b34ee11627e8230e50
Forrest Collman <forrest.collman@gmail.com>
2020-06-26 07:13:33 -0700

    fixing table names

--- annotationengine/views.py
+++ annotationengine/views.py
@@ -2,6 +2,7 @@ from flask import jsonify, render_template, current_app, make_response, Blueprin
 from .aligned_volume import get_aligned_volumes
 from .anno_database import get_db
 from dynamicannotationdb.models import Metadata
+from dynamicannotationdb.key_utils import get_table_name_from_table_id
 import pandas as pd
 import os
 __version__ = "1.0.6"
@@ -21,7 +22,7 @@ def index():
 def aligned_volume_view(aligned_volume_name):
     
     db = get_db(aligned_volume_name)
-    tables = db.get_existing_tables()
+    table_names = db.get_existing_tables()
     query=db.session.query(Metadata).filter(Metadata.deleted == None)
     df = pd.read_sql(query.statement, db._client.engine)
     base_user_url = "https://{auth_uri}/api/v1/user/{user_id}"
@@ -32,10 +33,10 @@ def aligned_volume_view(aligned_volume_name):
                                                                          user_id=x.user_id),
                                                     x.user_id),
                        axis=1)
-    df['table_name']=df['table_name'].map(lambda x: x.split('__')[-1])
+    df['table_name']=df['table_name'].map(lambda x: get_table_name_from_table_id(x))
     return render_template('aligned_volume.html',
                             df_table=df.to_html(escape=False),
-                            tables=tables,
+                            tables=table_names,
                             aligned_volume_name=aligned_volume_name,
                             version=__version__)
 
