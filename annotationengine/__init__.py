0ac2f4685db92d7833087eacb4e984465915aa07
Forrest Collman <forrest.collman@gmail.com>
2021-06-05 10:44:03 -0700

    fixing swagger and create table schemas

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, url_for, redirect, Blueprint
 import json
+from flask_marshmallow import Marshmallow
+from marshmallow.utils import _Missing
 import numpy as np
 from emannotationschemas.models import Base
 from annotationengine.config import configure_app
@@ -17,6 +19,7 @@ __version__ = "2.1.4"
 
 db = SQLAlchemy(model_class=Base)
 
+
 class AEEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
@@ -25,15 +28,23 @@ class AEEncoder(json.JSONEncoder):
             return int(obj)
         if isinstance(obj, (datetime, date)):
             return obj.isoformat()
+        if isinstance(obj, _Missing):
+            return None
         return json.JSONEncoder.default(self, obj)
 
 
 def create_app(test_config=None):
     # Define the Flask Object
-    app = Flask(__name__,
-                instance_path=get_instance_folder_path(),
-                static_url_path='/annotation/static',
-                instance_relative_config=True)
+    app = Flask(
+        __name__,
+        instance_path=get_instance_folder_path(),
+        static_url_path="/annotation/static",
+        instance_relative_config=True,
+    )
+    from .schemas import ma
+
+    ma.init_app(app)
+
     app.json_encoder = AEEncoder
     app.config["RESTX_JSON"] = {"cls": AEEncoder}
     logging.basicConfig(level=logging.DEBUG)
@@ -43,25 +54,26 @@ def create_app(test_config=None):
     else:
         app.config.update(test_config)
 
-    apibp = Blueprint('api', __name__, url_prefix='/annotation/api')
-    @apibp.route('/versions')
+    apibp = Blueprint("api", __name__, url_prefix="/annotation/api")
+
+    @apibp.route("/versions")
     def versions():
         return jsonify([2]), 200
-        
+
     with app.app_context():
         api = Api(apibp, title="Annotation Engine API", version=__version__, doc="/doc")
-        api.add_namespace(api_bp, path='/v2')
+        api.add_namespace(api_bp, path="/v2")
         app.register_blueprint(apibp)
         app.register_blueprint(views_bp)
         db.init_app(app)
         db.create_all()
 
-
     @app.route("/health")
     def health():
         return jsonify("healthy"), 200
-   
+
     @app.route("/annotation/")
     def index():
         return redirect("/annotation/views")
+
     return app
--- annotationengine/schemas.py
+++ annotationengine/schemas.py
@@ -1,33 +1,46 @@
 from marshmallow import fields, Schema, post_load
 from flask_marshmallow import Marshmallow
-from dynamicannotationdb import models 
+from dynamicannotationdb import models
 
 ma = Marshmallow()
+
 
 class FullMetadataSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = models.AnnoMetadata
 
+
 class MetadataSchema(Schema):
-    user_id = fields.Str(required=False)
-    description = fields.Str(required=True)
-    reference_table = fields.Str(required=False)
-    flat_segmentation_source = fields.Str(required=False)
-    voxel_resolution_x = fields.Float(required=True)
-    voxel_resolution_y = fields.Float(required=True)
-    voxel_resolution_z = fields.Float(required=True)
-    
+    user_id = fields.Str(required=False, example="1")
+    description = fields.Str(
+        required=True, example="my annotation table to track cells made by John Doe"
+    )
+    reference_table = fields.Str(required=False, example="synapse_table")
+    flat_segmentation_source = fields.Str(
+        required=False, example="precomputed://gs://my_cloud_bucket/image"
+    )
+    voxel_resolution_x = fields.Float(required=True, example=1.0)
+    voxel_resolution_y = fields.Float(required=True, example=1.0)
+    voxel_resolution_z = fields.Float(required=True, example=1.0)
+
+
 class TableSchema(Schema):
-    table_name = fields.Str(order=0, required=True)   
-    schema_type = fields.Str(order=1, required=True)
+    table_name = fields.Str(order=0, required=True, example="my_cell_type_table")
+    schema_type = fields.Str(order=1, required=True, example="cell_type_local")
+
 
 class CreateTableSchema(TableSchema):
-    metadata = fields.Nested(MetadataSchema, order=3, required=True, example={'description': "my description"})
+    metadata = fields.Nested(
+        MetadataSchema,
+        order=3,
+        required=True,
+        example={"description": "my description"},
+    )
 
 
 class DeleteAnnotationSchema(Schema):
-    annotation_ids = fields.List(fields.Int, required=True)
-    
+    annotation_ids = fields.List(fields.Int, required=True, example=[1, 2, 5])
+
 
 class PutAnnotationSchema(Schema):
     annotations = fields.List(fields.Dict, required=True)
