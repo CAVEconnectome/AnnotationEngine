34fda0301117a2aba6ba4810276bd77a722b7ba9
Forrest Collman <forrest.collman@gmail.com>
2021-06-24 15:26:19 -0700

    using new middle auth

--- annotationengine/views.py
+++ annotationengine/views.py
@@ -19,6 +19,7 @@ import os
 from middle_auth_client import (
     auth_required,
     auth_requires_permission,
+    user_has_permission
 )
 from middle_auth_client.decorators import dataset_from_table_id
 
@@ -57,18 +58,6 @@ def fix_wkb_columns(df, convert_to_nm=None):
                 df[colname] = df[colname].apply(wkb_to_numpy, convert_to_nm)
     return df
 
-
-def user_has_permission(permission, table_id, resource_namespace, service_token=None):
-    token = (
-        service_token
-        if service_token
-        else flask.current_app.config.get("AUTH_TOKEN", "")
-    )
-    dataset = dataset_from_table_id(resource_namespace, table_id, token)
-    has_permission = permission in flask.g.auth_user.get("permissions_v2", {}).get(
-        dataset, []
-    )
-    return has_permission
 
 
 @views_bp.route("/")
