8c0dda60564599618b02addbdaf3dfc64a79647b
Forrest Collman <forrest.collman@gmail.com>
2021-08-19 05:53:18 -0700

    removing twine upload from script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,5 @@
 bumpversion $1
 git push && git push --tags
 TAG=$(git describe --tags)
-python setup.py sdist
-twine upload dist/annotationengine-${TAG:1}.tar.gz
+#python setup.py sdist
+#twine upload dist/annotationengine-${TAG:1}.tar.gz
