10dccb3b5e72e2436142a55800c77a6e7c874dfb
sdorkenw <sven.dorkenwald@googlemail.com>
2021-11-11 15:39:23 -0800

    Bump version: 3.3.4 → 3.3.5

--- annotationengine/api.py
+++ annotationengine/api.py
@@ -31,7 +31,7 @@ import logging
 from enum import Enum
 from typing import List
 
-__version__ = "3.3.4"
+__version__ = "3.3.5"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
