233dc230527ab3101dbd8d9f059bed9540dead3c
Forrest Collman <forrest.collman@gmail.com>
2022-02-01 08:39:42 -0800

    Bump version: 3.5.0 → 3.5.1

--- annotationengine/api.py
+++ annotationengine/api.py
@@ -22,7 +22,7 @@ from multiwrapper import multiprocessing_utils as mu
 import requests
 import logging
 
-__version__ = "3.5.0"
+__version__ = "3.5.1"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
