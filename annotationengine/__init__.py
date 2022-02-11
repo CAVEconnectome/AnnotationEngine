cf74587d351bed6f1a53010fe0f7c195109ea2dd
Derrick Brittain <dlbrittain@gmail.com>
2022-02-11 10:29:21 -0800

    Bump version: 3.5.2 → 3.5.3

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -14,7 +14,7 @@ from flask_restx import Api
 import logging
 from datetime import date, datetime
 
-__version__ = "3.5.2"
+__version__ = "3.5.3"
 
 
 db = SQLAlchemy(model_class=Base)
--- annotationengine/views.py
+++ annotationengine/views.py
@@ -17,7 +17,7 @@ import pandas as pd
 import numpy as np
 import os
 
-__version__ = "3.5.2"
+__version__ = "3.5.3"
 
 views_bp = Blueprint("views", __name__, url_prefix="/annotation/views")
 
