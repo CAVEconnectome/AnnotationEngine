6ec8aefe554a05200527d8340b325970ef7469bb
Forrest Collman <forrest.collman@gmail.com>
2022-02-20 20:50:50 -0800

    Bump version: 3.6.2 → 3.6.3

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -14,7 +14,7 @@ from flask_restx import Api
 import logging
 from datetime import date, datetime
 
-__version__ = "3.6.2"
+__version__ = "3.6.3"
 
 
 db = SQLAlchemy(model_class=Base)
--- annotationengine/api.py
+++ annotationengine/api.py
@@ -22,7 +22,7 @@ from multiwrapper import multiprocessing_utils as mu
 import requests
 import logging
 
-__version__ = "3.6.2"
+__version__ = "3.6.3"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
