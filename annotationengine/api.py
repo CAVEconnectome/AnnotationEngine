359acbb139a245fc6bbf1df2a3d32c8ec506da8c
Forrest Collman <forrest.collman@gmail.com>
2022-06-27 12:56:04 -0700

    Bump version: 4.0.5 → 4.0.6

--- annotationengine/api.py
+++ annotationengine/api.py
@@ -20,7 +20,7 @@ from multiwrapper import multiprocessing_utils as mu
 
 from .api_examples import synapse_table_example
 
-__version__ = "4.0.5"
+__version__ = "4.0.6"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
