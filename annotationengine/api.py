8493f312f0e648f0207b683cd1bc83356bd3b5b3
Forrest Collman <forrest.collman@gmail.com>
2022-09-29 16:09:13 -0700

    Bump version: 4.1.0 → 4.1.1

--- annotationengine/api.py
+++ annotationengine/api.py
@@ -23,7 +23,7 @@ from annotationengine.schemas import (
 
 from .api_examples import synapse_table_example
 
-__version__ = "4.1.0"
+__version__ = "4.1.1"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
