cb576322f2e71fb21a11ec0b4b0800b34ef98d5e
Forrest Collman <forrest.collman@gmail.com>
2023-01-29 20:03:02 -0800

    fix: dadb interface change

--- test/test_anno_database.py
+++ test/test_anno_database.py
@@ -6,4 +6,4 @@ def test_create_db(app_config, mocker):
     with app_config.app_context():
         db = get_db("test_aligned_volume")
         logging.info(db.__dict__)
-        assert db.__dict__["aligned_volume"] == "test_aligned_volume"
+        assert db.aligned_volume == "test_aligned_volume"
