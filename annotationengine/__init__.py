7c7ed550d45cc2c0c69a4a3b243f1e534b81390a
Derrick Brittain <dlbrittain@gmail.com>
2023-02-14 14:22:54 -0800

    Bump version: 4.10.0 → 4.10.1

--- annotationengine/__init__.py
+++ annotationengine/__init__.py
@@ -14,7 +14,7 @@ from flask_restx import Api
 import logging
 from datetime import date, datetime
 
-__version__ = "4.10.0"
+__version__ = "4.10.1"
 
 
 db = SQLAlchemy(model_class=Base)
--- annotationengine/api.py
+++ annotationengine/api.py
@@ -40,7 +40,7 @@ from annotationengine.schemas import (
 
 from .api_examples import synapse_table_example, synapse_table_update_example
 
-__version__ = "4.10.0"
+__version__ = "4.10.1"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
